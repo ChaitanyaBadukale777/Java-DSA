897833a0eaa1c479ecdea66c9efda9e565338bcc
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-09-28 14:16:05 +0000

    default constructor added

---
+++ Basic_Programs/MainOne.java
@@ -0,0 +1,13 @@
+// Default Constructor
+
+class Car {
+    Car() {
+        System.out.println("Car is created!");
+    }
+}
+
+public class MainOne {
+    public static void main(String args[]) {
+        Car c1 = new Car(); //Automatically calls the constructor
+    }
+}
