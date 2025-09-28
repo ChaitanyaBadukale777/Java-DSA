d2f322dc550793ee73e4aa7ba60cd48cde7c8f14
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-09-28 13:47:36 +0000

    changes done

---
+++ Basic_Programs/Main.java
@@ -0,0 +1,23 @@
+class Student {
+    String name;
+    int age;
+
+    // Constructor
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // constructor automatically sets values when we create the object
+        Student s1 = new Student("Chaitanya", 21);
+        s1.display();
+    }
+}
