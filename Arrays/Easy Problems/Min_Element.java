f0ca4a6b61693d8db46c474e463149609e1dd592
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-10-03 17:39:02 +0000

    min element in array program add

---
+++ Arrays/Easy Problems/Min_Element.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String args[])
+    {
+        int a[] = {23, 19, 44, 10, 12, 25, 30, 49, 17, 22};
+        
+        int min = a[0];
+        for(int i = 0; i < a.length; i++)
+        {
+            int var = a[i];
+            if(min > var)
+                min = var;
+        }
+        System.out.println(min);
+    }
+}
