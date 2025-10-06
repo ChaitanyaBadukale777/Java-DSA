771610acf755a35729b82390a8b4bd223f9b90b0
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-10-06 12:08:54 +0000

    missing number program added

---
+++ Basic_Programs/MissingNumber.java
@@ -0,0 +1,15 @@
+//  WAP to find the missing number from array
+
+import java.util.Arrays;
+class MissingNumber {
+    public static void main(String[] args) {
+        int a[] = {20, 21, 22, 24, 25, 26, 27};
+        Arrays.sort(a);
+        int sum = 0;
+        for(int i = a[0]; i <= a[a.length - 1]; i++) 
+            sum += i;
+        for(int i = 0; i < a.length; i++)
+            sum -= a[i];
+        System.out.println("Missing number is: " + sum);
+    }
+}
