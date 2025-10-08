1422ebcd5fefba77af53da1f0d127ad67ad20428
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-10-08 18:49:12 +0000

    merge 2 array problem

---
+++ Arrays/Easy_Problems/Merge_Two_Array.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Merge_Two_Array {
+    public static void main(String[] args) {
+        int a[] = {10, 20, 30, 40, 50, 60};
+        int b[] = {11, 22, 33, 44};
+
+        int c[] = new int[a.length + b.length];   
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i];
+        }
+
+        for (int i = 0; i < b.length; i++) {
+            c[a.length + i] = b[i];
+        }
+
+        System.out.println(Arrays.toString(c));
+    }
+}
