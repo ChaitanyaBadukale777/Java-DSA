0df0b209475e6e264fcacabeab56447834cfbe83
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2025-11-03 17:03:53 +0000

    commit

---
+++ OOP/Encapsulation/Car.java
@@ -0,0 +1,26 @@
+class Car {
+    private int speed; // Private variable
+
+    // Setter method (to set value safely)
+    public void setSpeed(int s) {
+        if (s >= 0 && s <= 200) // Validation
+            speed = s;
+        else
+            System.out.println("Invalid speed. Please set a speed between 0 and 200.");
+    }
+
+    // Getter method (to read value)
+    public int getSpeed() {
+        return speed;
+    }
+}
+
+public class Car {
+    public static void main(String args[]) {
+        Car c = new Car();
+
+        c.setSpeed(150); // Valid speed
+        System.out.println("Current speed: " + c.getSpeed()); // Getter
+        c.setSpeed(250); // Invalid speed
+    }
+}
