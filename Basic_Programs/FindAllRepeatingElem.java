811ab9b6802aea2f33abceb4a868c5767ac6f515
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2026-04-12 17:39:31 +0530

    commit

---
+++ Basic_Programs/FindAllRepeatingElem.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class FindAllRepeatingElem {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 4, 1, 5};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // count frequency
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // print repeating elements
+        for (int key : map.keySet()) {
+            if (map.get(key) > 1) {
+                System.out.print(key + " ");
+            }
+        }
+    }
+}
