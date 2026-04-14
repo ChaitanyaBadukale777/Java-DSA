ac3cc45e05116ce2716d3206255591cf3c773f6f
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2026-04-14 15:49:36 +0530

    linear search added

---
+++ Algorithms/Searching_Algorithms/LinearSearch.java
@@ -0,0 +1,15 @@
+class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 7, 1, 9};
+        int target = 7;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Found at index: " + i);
+                return;
+            }
+        }
+
+        System.out.println("Not Found");
+    }
+}
