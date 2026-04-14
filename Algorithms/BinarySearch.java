b171baaf29e68865351f07ca976fb406b5512c98
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2026-04-14 15:42:20 +0530

    commit

---
+++ Algorithms/BinarySearch.java
@@ -0,0 +1,25 @@
+class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11};
+        int target = 7;
+
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                System.out.println("Found at index: " + mid);
+                return;
+            } 
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println("Not Found");
+    }
+}
