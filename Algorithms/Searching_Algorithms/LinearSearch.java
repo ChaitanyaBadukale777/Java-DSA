1dcd81926ea2442f7a66c361881f2bb8052c1a7c
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2026-04-14 16:27:07 +0530

    commit

--- Algorithms/Searching_Algorithms/LinearSearch.java
+++ Algorithms/Searching_Algorithms/LinearSearch.java
@@ -13,3 +13,13 @@ class LinearSearch {
         System.out.println("Not Found");
     }
 }
+
+// Linear Search sequentially checks each element of the array until the target is found or the array ends.
+// It does not require the data to be sorted and is simple to implement. The time complexity is O(n) in the worst and average case,
+// and O(1) in the best case when the element is found at the beginning. It is efficient for small or unsorted datasets but not suitable for large inputs.
+
+// Linear Search has:
+
+// 👉 Best Case: O(1) — element found at first position
+// 👉 Average Case: O(n) — element may be anywhere
+// 👉 Worst Case: O(n) — element at last or not present
