601a11767ca01d48251fbed8af53753d4a527089
Chaitanya <109964522+ChaitanyaBadukale777@users.noreply.github.com>
2026-08-10 01:03:33 +0530

    Add NonRepeatingChar class to find first non-repeating character

---
+++ String/NonRepeatingChar.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class NonRepeating {
+    public static void main(String[] args) {
+        String str = "aabbcde";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int count = 0;
+
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == ch) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println("First Non-Repeating: " + ch);
+                break;
+            }
+        }
+    }
+}
